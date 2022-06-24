e959d6153932b532e3af60941d5a6d5a5d203c61
ahmad.al-naib <ahmad.al-naib@sitec.de>
2022-06-24 14:19:30 +0200

    adad

---
+++ htmltopdf/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="event-detail">
+    <h1>Thank you for registering with us.</h1>
+    <h2>This is the acknowledgement of your registeration for attending the
+      event.</h2>
+    <p class="row">
+      Event Name:<br />Machine learning introduction for kids
+    </p>
+    <p class="row">
+      Event Date:<br />27 May 2021
+    </p>
+    <p class="row">
+      Time:<br />10:30 AM
+    </p>
+    <p class="row">
+      Venue:<br />AMC Graduate Center,<br>25, AMC Street,<br>New York, USA.
+    </p>
+    <p class="pdf-content">
+      Contact our <a href="#">Organizers' Team</a> if you need any support.
+    </p>
+  </div>
+
+</body>
+</html>
