652cc3156c4d77f235ce87dfde86f853f9072d50
ahmad.al-naib <ahmad.al-naib@sitec.de>
2022-06-28 17:24:50 +0200

    add search functions

--- locktec-d-one/app.js
+++ locktec-d-one/app.js
@@ -81,3 +81,157 @@ const swiper = new Swiper('.swiper', {
 });
 
 
+window.addEventListener('scroll',(e)=>{
+  const nav = document.querySelector('nav');
+  if(window.pageYOffset>0){
+    nav.classList.add("shadow-sm");
+  }else{
+    nav.classList.remove("shadow-sm");
+  }
+});
+
+
+
+let searchIndex = [
+	{
+		title: "Locktec",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+    summary: "A short summary or preview of the content (can also be a clipped version of the first few sentences)..."
+
+	},
+	{
+		title: "Locktec",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+    summary: "A short summary or preview of the content ..."
+
+	},
+	{
+		title: "Locktec",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+    summary: "A "
+
+	},
+	{
+		title: "servicebox",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+
+	},
+	{
+		title: "contect",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+
+	},
+	{
+		title: "kontakt",
+		url: "https://www.locktec.com/produkte/cool-lockers/",
+
+	},
+	// More content...
+];
+
+(function (window, document, undefined) {
+
+	'use strict';
+
+	//
+	// Variables
+	//
+
+	let form = document.querySelector('#form-search');
+	let input = document.querySelector('#input-search');
+	let resultList = document.querySelector('#search-results');
+
+
+
+
+	//
+	// Methods
+	//
+
+	/**
+	 * Create the HTML for each result
+	 * @param  {Object} article The article
+	 * @param  {Number} id      The result index
+	 * @return {String}         The markup
+	 */
+	let createHTML = function (article, id) {
+		let html =
+			'<div id="search-result-' + id + '">' +
+				'<a href="' + article.url + '">' +
+				
+					'<h2>' + article.title + '</h2>' +
+          '<p>' +article.summary  +'</p>'
+				
+				'</a>' +
+			'</div>';
+		return html;
+	};
+
+	/**
+	 * Create the markup when no results are found
+	 * @return {String} The markup
+	 */
+	let createNoResultsHTML = function () {
+		return '<p>Sorry, no matches were found.</p>';
+	};
+
+	/**
+	 * Create the markup for results
+	 * @param  {Array} results The results to display
+	 * @return {String}        The results HTML
+	 */
+	let createResultsHTML = function (results) {
+		let html = '<p>Found ' + results.length + ' matching articles</p>';
+		html += results.map(function (article, index) {
+			return createHTML(article, index);
+		}).join('');
+		return html;
+	};
+
+	/**
+	 * Search for matches
+	 * @param  {String} query The term to search for
+	 */
+	let search = function (query) {
+
+		// Variables
+		let reg = new RegExp(query, 'gi');
+		let priority1 = [];
+		let priority2 = [];
+		let priority3 = [];
+
+		// Search the content
+		searchIndex.forEach(function (article) {
+			if (reg.test(article.title)) return priority1.push(article);
+			if (reg.test(article.title)) priority2.push(article);
+			if (reg.test(article.title)) priority3.push(article);
+		});
+
+		// Combine the results into a single array
+		let results = [].concat(priority1, priority2,priority3);
+
+		// Display the results
+		resultList.innerHTML = results.length < 1 ? createNoResultsHTML() : createResultsHTML(results);
+
+	};
+
+	/**
+	 * Handle submit events
+	 */
+	let submitHandler = function (event) {
+		event.preventDefault();
+		search(input.value);
+	};
+
+
+	//
+	// Inits & Event Listeners
+	//
+
+	// Make sure required content exists
+	if (!form || !input || !resultList || !searchIndex ) return;
+
+	// Create a submit handler
+	form.addEventListener('keyup', submitHandler);
+
+})(window, document);
